e872fd923027d7351ab9aa643b9ed4fa7b6ffd01
calebpenning <calebpenning@mail.com>
2022-01-06 15:55:15 -0800

    added scripts to combine nodemon and tsc watch while in development

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.npm_package_config_port || 5000;
 let runningMessage = `Server is running on port ${PORT}`;
 app.get('/', (req, res) => {
     console.log(`API request from ${req.ip}`);
-    res.send(runningMessage + "hello!");
+    res.send(runningMessage + " yoooooooo!");
 });
 const server = app.listen(PORT, () => {
     console.log(runningMessage);
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.npm_package_config_port || 5000
 let runningMessage = `Server is running on port ${PORT}`
 
 app.get('/', (req, res) => {
     console.log(`API request from ${req.ip}`)
-    res.send(runningMessage + "hello!")
+    res.send(runningMessage + " yoooooooo!")
 })
 
 const server = app.listen(PORT, () => {
